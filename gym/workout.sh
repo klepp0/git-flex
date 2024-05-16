ac7fa802a52c5cc8a18345f48daa360d4a588c65
sacklippe <jonas.kleppich@voltfang.de>
2024-05-16 16:38:27 +0200

    feat: add list of random phrases added to the commit message

--- gym/workout.sh
+++ gym/workout.sh
@@ -4,19 +4,24 @@ GYM_PATH="$(readlink -f "$0")"
 GYM_DIR="$(dirname "$GYM_PATH")"
 REPO_DIR="$(dirname "$GYM_DIR")"
 LIFTS_PATH="$GYM_DIR/lifts.txt"
+PHRASES_PATH="$GYM_DIR/phrases.txt"
+
+if [[ -f "$PHRASES_PATH" ]]; then
+    NUM_LINES=$(wc -l < "$PHRASES_PATH")
+    RANDOM_LINE_NUM=$((RANDOM % NUM_LINES + 1))
+    PHRASE=$(awk "NR==$RANDOM_LINE_NUM" "$PHRASES_PATH")
+else
+    echo "File $PHRASES_PATH does not exist."
+fi
 
 read -r NUM_LIFTS < "$LIFTS_PATH"
-
 echo "Current number of repetitions: "$NUM_LIFTS""
-echo "🏋️ Let's lift it"
-
 echo $((NUM_LIFTS + 1)) > "$LIFTS_PATH"
 read -r NUM_LIFTS < "$LIFTS_PATH"
-
 echo "Current number of repetitions: $NUM_LIFTS"
 
 cd "$REPO_DIR"
 git add gym/lifts.txt
-git commit -m "refactor: 🏋️ let's lift it"
+git commit -m "refactor: $PHRASE"
 git push origin main
 cd "$CWD"
